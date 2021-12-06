9965ba6aad5050ff3e3c3d9c423e9275813bf07e
Daniel Dutra <danieldutra@frwk.com.br>
2021-12-06 15:39:08 -0300

    Implementing categories CRUD

--- src/main/java/com/drugstore/api/controller/CategoryController.java
+++ src/main/java/com/drugstore/api/controller/CategoryController.java
@@ -1,14 +1,21 @@
 package com.drugstore.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,14 +40,55 @@ public class CategoryController {
 		return categoryRepository.findAll();
 	}
 	
+	@GetMapping("/{idCategory}")
+	public ResponseEntity<?> findById(@PathVariable Long idCategory) {
+		Optional<Category> categoryOpt = categoryRepository.findById(idCategory);
+		
+		if(categoryOpt.isPresent()) {
+			return ResponseEntity.ok(categoryOpt.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
 	@PostMapping
-	public ResponseEntity<?> create(@RequestBody @Valid Category category) throws Exception {
+	public ResponseEntity<?> create(@RequestBody @Valid Category category) {
 		try {
 			category = categoryService.create(category);
 			return ResponseEntity.status(HttpStatus.CREATED).body(category);
 		
 		} catch (AlreadyExistingCategoryException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@PutMapping("/{idCategory}")
+	public ResponseEntity<?> update(@PathVariable Long idCategory, @RequestBody @Valid Category category) {
+		Optional<Category> categoryOpt = categoryRepository.findById(idCategory);
+		
+		if(!categoryOpt.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		Category storedCategory = categoryOpt.get();
+		BeanUtils.copyProperties(category, storedCategory, "id", "createdAt", "updatedAt");
+		
+		storedCategory = categoryRepository.save(storedCategory);
+		
+		return ResponseEntity.ok(storedCategory);
+	}
+	
+	@DeleteMapping("/{idCategory}")
+	public ResponseEntity<?> delete(@PathVariable Long idCategory) {
+		try {
+			categoryRepository.deleteById(idCategory);
+			return ResponseEntity.noContent().build();
+
+		} catch (EmptyResultDataAccessException e) {
+			return ResponseEntity.notFound().build();
+			
+		} catch (DataIntegrityViolationException e) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Category can't be removed.");
 		}
 	}
 }
--- src/main/java/com/drugstore/api/domain/service/CategoryService.java
+++ src/main/java/com/drugstore/api/domain/service/CategoryService.java
@@ -24,4 +24,5 @@ public class CategoryService {
 		
 		return categoryRepository.save(category);
 	}
+	
 }
