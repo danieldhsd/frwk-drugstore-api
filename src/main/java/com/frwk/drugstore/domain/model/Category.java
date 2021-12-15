279168de5194d9e93564fcbb26d7d1b1e870db31
Daniel Dutra <danieldutra@frwk.com.br>
2021-12-15 13:41:35 -0300

    Adding lombok dependency in project

--- src/main/java/com/frwk/drugstore/domain/model/Category.java
+++ src/main/java/com/frwk/drugstore/domain/model/Category.java
@@ -14,8 +14,13 @@ import javax.validation.constraints.NotBlank;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @Table(name = "tb_category")
+@Getter
+@Setter
 public class Category {
 
 	@Id
@@ -56,45 +61,6 @@ public class Category {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public OffsetDateTime getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(OffsetDateTime createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	public OffsetDateTime getUpdatedAt() {
-		return updatedAt;
-	}
-
-	public void setUpdatedAt(OffsetDateTime updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
 
 }
+
